a410bda321d7c804c67b4f51b75188c2fc56a893
Maas-Maarten Zeeman <mmzeeman@xs4all.nl>
2012-12-16 16:10:25 +0100

    Added some documentation

--- src/reggy.erl
+++ src/reggy.erl
@@ -22,33 +22,64 @@
 
 -export([start/1]).
 
+% callback functions for registering with {via, reggy, {RegistryName, Name}}
 -export([register_name/2, unregister_name/1, whereis_name/1, send/2]).
 
 
-% @doc Start a registry. 
+%% @doc Start a registry. It will be available under Name. 
+%%
+-spec start(Name) -> {ok, Pid} when
+      Name :: atom() | {local, atom()} | {global, atom()} | {via, module(), atom()},
+      Pid :: pid().
 start(Name) ->
 	reggy_sup:start_reg(Name).
 
-% @doc callback function
-%
-% For registering via... {via, reggy, {RegistryName, Name}}
-register_name({RegistryName, Name}, Pid) ->
-	case reggy_reg:register(RegistryName, Name, Pid) of
+
+
+%% @doc Register Name on Registry. Returns 'yes' iff the process was registered.
+%%
+-spec register_name({Registry, Name}, Pid) -> 'yes' | 'no' when
+	  Registry :: atom(),
+      Name :: term(),
+      Pid :: pid().
+register_name({Registry, Name}, Pid) ->
+	case reggy_reg:register(Registry, Name, Pid) of
 		ok -> yes;
 		_ -> no
 	end.
 
-unregister_name({RegistryName, Name}) ->
-	reggy_reg:unregister(RegistryName, Name).
 
+%% @doc Unregister the process with Name.
+%%
+-spec unregister_name({Registry, Name}) -> _ when
+      Registry :: atom(),
+      Name :: term().
+unregister_name({Registry, Name}) ->
+	reggy_reg:unregister(Registry, Name).
+
+
+%% @doc Lookup the pid of Name on Registry.
+%%
+-spec whereis_name({Registry, Name}) -> pid() | 'undefined' when
+      Registry :: atom(),
+      Name :: term().
 whereis_name({RegistryName, Name}) ->
 	reggy_reg:whereis(RegistryName, Name).
 
-send({RegistryName, Name}=N, Msg) ->
-	case reggy_reg:whereis(RegistryName, Name) of
+
+%% @doc Send Msg to Name available on Registry.
+%%
+-spec send({Registry, Name}, Msg) -> Pid when
+      Registry :: atom(),
+      Name :: term(),
+      Msg :: term(),
+      Pid :: pid().
+send({Registry, Name}=N, Msg) ->
+	case reggy_reg:whereis(Registry, Name) of
 		undefined ->
 	 		exit({badarg, {N, Msg}});
 	 	Pid when is_pid(Pid) ->
 	 		Pid ! Msg,
 	 		Pid
 	end.
+	
